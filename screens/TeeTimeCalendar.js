54030046b5667fdac09ed03f5a2502864932986b
davidleegriffin <davidleegriffin@yahoo.com>
2021-07-10 21:09:41 -0500

    end of day

--- screens/TeeTimeCalendar.js
+++ screens/TeeTimeCalendar.js
@@ -23,7 +23,7 @@ function TeeTimeCalendar({route, navigation}) {
     return (
         <>
             <View style={styles.container}>
-                <Text>CALENDAR</Text>
+                {/* <Text>CALENDAR</Text> */}
                 <CalendarList
                 style={{
                     paddingTop: 100,
@@ -32,17 +32,30 @@ function TeeTimeCalendar({route, navigation}) {
                 }}
                 markingType={'custom'}
                 markedDates={{ 
-                    '2021-07-12': {
+                    '2021-07-16': {
                         customStyles: { 
                             container: {
-                                backgroundColor: 'green' 
+                                backgroundColor: 'rgba(55,205,55,0.65)',
+                                borderWidth: 1,
                             },
                         text: {
                             color: 'black',
                             fontWeight: 'bold'
                             }
                         }
-                    }
+                    },
+                    '2021-07-12': {
+                        customStyles: { 
+                            container: {
+                                backgroundColor: 'lightyellow',
+                                borderWidth: 1,
+                            },
+                        text: {
+                            color: 'black',
+                            fontWeight: 'bold'
+                            }
+                        }
+                    },
                 }}
                 showWeekNumbers={true}
                   // Enable horizontal scrolling, default = false
@@ -65,6 +78,27 @@ function TeeTimeCalendar({route, navigation}) {
                 minDate={new Date()}
                 onDayPress={(day) => {getAgenda(day)}}
                 />
+                <Agenda
+                // The list of items that have to be displayed in agenda. If you want to render item as empty date
+                // the value of date key has to be an empty array []. If there exists no value for date key it is
+                // considered that the date in question is not yet loaded
+                onDayPress={(day) => {console.log('day pressed', day)}}
+                items={{
+                    '2021-07-11': [{name: 'item 1 - any js object'}],
+                    '2021-07-13': [{name: 'item 2 - any js object', height: 80}],
+                    '2021-07-14': [],
+                    '2021-07-15': [{name: 'item 3 - any js object'}, {name: 'any js object'}]
+                }}
+                theme={{
+                    // ...calendarTheme,
+                    agendaDayTextColor: 'black',
+                    agendaDayNumColor: 'green',
+                    agendaTodayColor: 'red',
+                    agendaKnobColor: 'blue'
+                }}
+                  // Agenda container style
+                style={{}}
+                />
             </View>
         </>
     )
@@ -73,8 +107,9 @@ function TeeTimeCalendar({route, navigation}) {
 const styles=StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        height: 100,
     },
 });
 export default TeeTimeCalendar;
