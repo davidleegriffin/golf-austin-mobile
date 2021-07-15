fd78ca25665043d6d4a0b9707e37cdc35cde0e95
samuelghill <samuelgarretthill@gmail.com>
2021-07-15 09:19:52 -0500

    added stepzen fauna and google calendar schema

--- App.tsx
+++ App.tsx
@@ -25,9 +25,9 @@ const client = new ApolloClient({
 	link: createHttpLink({
 		credentials: "same-origin",
 		headers: {
-			Authorization: `Apikey headyflamingomouth::stepzen.net+1000::d603591e9b22ec93bbf3f204f018c484d285b3e4a26cb956a442d3a249d45414`,
+			Authorization: `Apikey {{api_key}}`,
 		},
-		uri: "https://HeadyFlamingoMouth.stepzen.net/api/warped-chimp/__graphql",
+		uri: "{{endpoint}}",
 	}),
 	cache: new InMemoryCache(),
 });
